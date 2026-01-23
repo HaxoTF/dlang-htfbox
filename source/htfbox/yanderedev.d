d3ee48539ac877256523b6b57c984371d777c0d4
HaxoTF <haxotf@proton.me>
2026-01-23 14:07:58 +0100

    source/yanderedev.d
    I wish there was an easier way to do this

---
+++ source/htfbox/yanderedev.d
@@ -0,0 +1,79 @@
+module htfbox.yanderedev;
+
+// It's here for as a joke
+// WARNING: Importing this module will shrink your pp
+
+bool is_even(int v)
+{
+    if (v==0) return true;
+    if (v==1) return false;
+    if (v==2) return true;
+    if (v==3) return false;
+    if (v==4) return true;
+    if (v==5) return false;
+    if (v==6) return true;
+    if (v==7) return false;
+    if (v==8) return true;
+    if (v==9) return false;
+    if (v==10) return true;
+    if (v==11) return false;
+    if (v==12) return true;
+    if (v==13) return false;
+    if (v==14) return true;
+    if (v==15) return false;
+    if (v==16) return true;
+    if (v==17) return false;
+    if (v==18) return true;
+    if (v==19) return false;
+    if (v==20) return true;
+    if (v==21) return false;
+    if (v==22) return true;
+    if (v==23) return false;
+    if (v==24) return true;
+    if (v==25) return false;
+    if (v==26) return true;
+    if (v==27) return false;
+    if (v==28) return true;
+    if (v==29) return false;
+    if (v==30) return true;
+    if (v==31) return false;
+    if (v==32) return true;
+    if (v==33) return false;
+    if (v==34) return true;
+    if (v==35) return false;
+    if (v==36) return true;
+    if (v==37) return false;
+    if (v==38) return true;
+    if (v==39) return false;
+    if (v==40) return true;
+    if (v==41) return false;
+    if (v==42) return true;
+    if (v==43) return false;
+    if (v==44) return true;
+    if (v==45) return false;
+    if (v==46) return true;
+    if (v==47) return false;
+    if (v==48) return true;
+    if (v==49) return false;
+    if (v==50) return true;
+    if (v==51) return false;
+    if (v==52) return true;
+    if (v==53) return false;
+    if (v==54) return true;
+    if (v==55) return false;
+    if (v==56) return true;
+    if (v==57) return false;
+    if (v==58) return true;
+    if (v==59) return false;
+    if (v==60) return true;
+    if (v==61) return false;
+    if (v==62) return true;
+    if (v==63) return false;
+    if (v==64) return true;
+    if (v==65) return false;
+    if (v==66) return true;
+    if (v==67) return false;
+    if (v==68) return true;
+    if (v==69) return false;
+    return false; // idk
+}
